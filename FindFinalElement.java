efcef62408eebfd6c96844606cdf104f6c630a9e
“srikanthvadde” <srikanthjobs2406@gmail.com>
2022-11-20 11:23:07 +0800

    [commit weekly assignment 1]

--- FindFinalElement.java
+++ FindFinalElement.java
@@ -1,3 +1,36 @@
 //FindFinalElement.java
 
-//public class 
+public class FindFinalElement {
+
+	public static void main(String[]args){
+
+		int[] array = new int[]{7,8,3,4,2,9,5};
+		System.out.println(findFinalElement(array));
+
+	}
+
+
+	private static int findFinalElement(int[] arr){
+
+	sortTheArray(arr);
+     return arr[arr.length%2==0 ? arr.length/2-1 : arr.length/2];
+	
+ }
+
+
+ private static void sortTheArray(int[] array){
+
+	for(int i=0;i<array.length;i++){
+	 for(int j=i+1;j<array.length;j++){
+		 if(array[i]>array[j]){
+		  int temp = array[i];
+		  array[i]=array[j];
+		  array[j]=temp;
+	  }
+
+  }
+}
+
+}
+
+} 
